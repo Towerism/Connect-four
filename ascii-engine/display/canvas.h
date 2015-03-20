41558ff016c88cd9f6d4a564b84c3955f6b21366
unknown <martin.frackerjr@gmail.com>
2015-03-20 01:18:33 -0500

    No idea

--- ascii-engine/display/canvas.h
+++ ascii-engine/display/canvas.h
@@ -6,7 +6,7 @@
 namespace ascii_engine {
     struct Canvas {
         Canvas(int width, int height, char val = ' ') : 
-            buffer(width, height, val) { }
+            buffer(width, height, val) { cout << width << height << endl; }
 
         void draw(int x, int y, char c) { buffer.set_char(x, y, c); }
         void draw(int x, int y, const string& s);
