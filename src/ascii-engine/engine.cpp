62b3bb034a60496f7928b4230ea2a554d4a4af09
unknown <martin.frackerjr@gmail.com>
2015-03-22 00:45:24 -0500

    Added input polling into game loop

--- src/ascii-engine/engine.cpp
+++ src/ascii-engine/engine.cpp
@@ -1,14 +1,18 @@
 #include "ascii-engine/engine.h"
 #include "ascii-engine/timing/framerate_limiter.h"
+#include "ascii-engine/input/input_handler.h"
 
 namespace ae = ascii_engine;
 using namespace std;
 
 void ae::Engine::game_loop() {
-    in_loop = true;
+    using Input_handler = ae::Input_handler;
     Framerate_limiter fps_limiter(target_fps);
+    Input_handler& input = Input_handler::get_instance();
+    in_loop = true;
     while (!exit_loop) {
         fps_limiter.frame_start();
+        input.poll();
         world->update(fps_limiter.get_delta_time());
         fps_limiter.frame_end();
     }
