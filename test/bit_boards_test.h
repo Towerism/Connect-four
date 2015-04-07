fbb8af0595b319c552ad86ef69b936d6e755fbd3
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-07 12:26:48 -0500

    Configured for google test

---
+++ test/bit_boards_test.h
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+struct BitBoardsTest : public ::testing::Test {
+protected:
+    BitBoardsTest() {
+        white_bb.set_adversary(black_bb);
+        black_bb.set_adversary(white_bb);
+    }
+
+    Bit_boards white_bb;
+    Bit_boards black_bb;
+}
